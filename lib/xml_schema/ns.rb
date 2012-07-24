db969fa1d6e97d4a0e8d6eaeee0ea1e165268ffa
Slava Kravchenko <slava.kravchenko@gmail.com>
2012-07-24 16:16:19 +0300

    custom namespaces can be added with "NS.add"; added "FOAF" and "DC" namespaces

--- lib/xml_schema/ns.rb
+++ lib/xml_schema/ns.rb
@@ -1,23 +1,19 @@
 require 'uri'
 
 module NS
-
-  ALL = {
-    'XMLSchema' => 'http://www.w3.org/2001/XMLSchema',
-    'OWL' => 'http://www.w3.org/2002/07/owl',
-    'RDF' => 'http://www.w3.org/1999/02/22-rdf-syntax-ns',
-    'RDFS' => 'http://www.w3.org/2000/01/rdf-schema'
-  }
-
-  ALL.each do |name, url|
+  # Add a resource namespace
+  #
+  # @param [String] name name of the module
+  # @param [String] uri URI of the resource
+  def self.add(name, uri)
     ns_module = self.const_set(name, Module.new)
     ns_module.module_eval <<-HERE
       def self.uri
-        URI(\"#{url}\")
+        URI(\"#{uri.sub(/\/?$/, '/')}\")
       end
 
       def self.to_s
-        \"#{url}\"
+        \"#{uri}\"
       end
 
       def self.[](value)
@@ -25,5 +21,11 @@ module NS
       end
 HERE
   end
-
 end
+
+NS.add 'XMLSchema', 'http://www.w3.org/2001/XMLSchema'
+NS.add 'OWL', 'http://www.w3.org/2002/07/owl'
+NS.add 'RDF', 'http://www.w3.org/1999/02/22-rdf-syntax-ns'
+NS.add 'RDFS', 'http://www.w3.org/2000/01/rdf-schema'
+NS.add 'FOAF', 'http://xmlns.com/foaf/0.1'
+NS.add 'DC', 'http://purl.org/dc'
